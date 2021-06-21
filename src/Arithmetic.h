110fde396ff6b38d97e28f3f3e74d36d2012f6e4
Prince-ranjan <59144550+Prince-ranjan@users.noreply.github.com>
2021-06-21 11:12:17 +0530

    Update Arithmetic.h

--- src/Arithmetic.h
+++ src/Arithmetic.h
@@ -1,9 +1,9 @@
 #include<stdio.h>
 #include <string.h>
-extern long long xreg[32];
-extern int isImm;
-extern long long xd, x1, x2, imm;
-void ADD(char *inst, int i)
+extern long long xreg[32]; // This will store the value of all 32 registers.
+extern int isImm; // This is used to check the persence of immediate Values.
+extern long long xd, x1, x2, imm; // distination,source1,source2,immediate.
+void ADD(char *inst, int i) // Arithematic Add operation.
 {
     getReg3Add(inst, i);
 	if(isImm)
@@ -11,7 +11,7 @@ void ADD(char *inst, int i)
 	else
 	xreg[xd] = xreg[x1]+xreg[x2];
 }
-void ADDI(char *inst, int i)
+void ADDI(char *inst, int i) // Arithematic Add Operation with Immediate
 {
     getReg3Add(inst, i);
 	if(!isImm)
@@ -19,7 +19,7 @@ void ADDI(char *inst, int i)
 	else
 	xreg[xd] = xreg[x1]+imm;
 }
-void SUB(char *inst, int i)
+void SUB(char *inst, int i) // Arithematic Subtract Operation
 {
     getReg3Add(inst, i);
 	if(isImm)
@@ -27,7 +27,7 @@ void SUB(char *inst, int i)
 	else
 	xreg[xd] = xreg[x1]-xreg[x2];
 }
-void MUL(char *inst, int i)
+void MUL(char *inst, int i) // Arithematic Multiple Operation
 {
     getReg3Add(inst, i);
 	if(isImm)
@@ -35,7 +35,7 @@ void MUL(char *inst, int i)
 	else
 	xreg[xd] = xreg[x1]*xreg[x2];
 }
-void MULI(char *inst, int i)
+void MULI(char *inst, int i) // Arithematic Multiple Operation with immediate.
 {
     getReg3Add(inst, i);
 	if(!isImm)
