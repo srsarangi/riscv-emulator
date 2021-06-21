444a56de25494a446e6d6ac8a3e6e16738c0b549
Smitanp <63653052+Smitanp@users.noreply.github.com>
2021-06-21 14:50:45 +0530

    Update Arithmetic.h

--- src/Arithmetic.h
+++ src/Arithmetic.h
@@ -3,7 +3,7 @@
 extern long long xreg[32];
 extern int isImm;
 extern long long xd, x1, x2, imm;
-void ADD(char *inst, int i)  // function that perform addition on content of two register and stores in destinstion register
+void ADD(char *inst, int i)  //Arguments:(instruction, index) | function that perform addition on content of two register and stores in destinstion register
 {
     getReg3Add(inst, i);  // calls getReg3Add() function that is present in Register.h header file
 	if(isImm)
@@ -11,7 +11,7 @@ void ADD(char *inst, int i)  // function that perform addition on content of two
 	else
 	xreg[xd] = xreg[x1]+xreg[x2];
 }
-void ADDI(char *inst, int i) // function that perform addition on content of first source register and immediate and stores in destinstion register
+void ADDI(char *inst, int i) // Arguments:(instruction, index) | function that perform addition on content of first source register and immediate and stores in destinstion register
 {
     getReg3Add(inst, i);  // calls getReg3Add() function that is present in Register.h header file
 	if(!isImm)
@@ -19,7 +19,7 @@ void ADDI(char *inst, int i) // function that perform addition on content of fir
 	else
 	xreg[xd] = xreg[x1]+imm;
 }
-void SUB(char *inst, int i) //function that perform substraction on content of two register and stores in destinstion register
+void SUB(char *inst, int i) //Arguments:(instruction, index) | function that perform substraction on content of two register and stores in destinstion register
 {
     getReg3Add(inst, i); // calls getReg3Add() function that is present in Register.h header file
 	if(isImm)
@@ -27,7 +27,7 @@ void SUB(char *inst, int i) //function that perform substraction on content of t
 	else
 	xreg[xd] = xreg[x1]-xreg[x2];
 }
-void MUL(char *inst, int i)  //function that perform multiplication on content of two source registers and stores in destinstion register
+void MUL(char *inst, int i)  // Arguments:(instruction, index) |function that perform multiplication on content of two source registers and stores in destinstion register
 {
     getReg3Add(inst, i);  //calls getReg3Add() function that is present in Register.h header file
 	if(isImm)
@@ -35,7 +35,7 @@ void MUL(char *inst, int i)  //function that perform multiplication on content o
 	else
 	xreg[xd] = xreg[x1]*xreg[x2];
 }
-void MULI(char *inst, int i) //function that perform multiplication on content of first source register and immediate and stores in destinstion register
+void MULI(char *inst, int i) // Arguments:(instruction, index) | function that perform multiplication on content of first source register and immediate and stores in destinstion register
 {
     getReg3Add(inst, i); // calls getReg3Add() function that is present in Register.h header file
 	if(!isImm)
